26bbc07ddb3b2ea0388c1f7360a7ed19fcff2360
Danish <danishnazir2001@gmail.com>
2022-06-14 16:50:47 +0530

    added validation in sign up

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,45 +1,71 @@
-import {React, useState} from 'react';
+import { React, useState } from 'react';
 import "./Signup.css"
 import { Link } from 'react-router-dom';
+import isEmail from 'validator/lib/isEmail';
+import isEmpty from 'validator/lib/isEmpty';
+import equals from 'validator/lib/equals';
+import { showErrorMsg, showSuccessMsg } from './../helpers/message';
+import { showLoading} from './../helpers/loading';
 
 const Signup = () => {
-  const [data,setData]=useState({
-    username:'',
-    email:'',
-    password:'',
-    cpassword:'',
+  const [data, setData] = useState({
+    username: '',
+    email: '',
+    password: '',
+    cpassword: '',
     successMsg: false,
     errorMsg: false,
     loading: false
   });
 
-const changeEvent = (e) => {
-  setData((prevData)=>(
-    {...prevData, [e.target.name]:e.target.value}
+  const { username, email, password, cpassword, successMsg, errorMsg, loading } = data;
+
+  const changeEvent = (e) => {
+    setData((prevData) => (
+      {
+        ...prevData,
+        [e.target.name]: e.target.value,
+        errorMsg: '',
+        successMsg: '',
+        loading: false
+      }
     ));
     console.log(data);
-}
+  }
+
+  const submitData = (e) => {
+    e.preventDefault();
+    if (isEmpty(username) || isEmpty(email) || isEmpty(password) || isEmpty(cpassword)) {
+      setData({ ...data, errorMsg: "All fields are required" });
+    } else if (!isEmail(email)) {
+      setData({ ...data, errorMsg: "Please enter a valid email" });
+    } else if (!equals(password, cpassword)) {
+      setData({ ...data, errorMsg: "Passwords does not match" });
+    } else {
+      setData({ ...data, successMsg: "Eid mubarakh oooooooooooo", loading: true });
+    }
+  }
   const showSignUp = () => (
-    <form className="container" onSubmit={(e)=>{ e.preventDefault()}} >
+    <form className="container" onSubmit={submitData} noValidate >
       <div className="row mb-3">
         <div className="col-sm-12">
-          <input value={data.username} onChange={changeEvent} name="username" type="text" className="form-control" id="text" placeholder="Username" />
+          <input value={username} onChange={changeEvent} name="username" type="text" className="form-control" id="text" placeholder="Username" />
         </div>
       </div>
       <div className="row mb-3">
         <div className="col-sm-12">
-          <input value={data.email} onChange={changeEvent} name="email" type="email" className="form-control" id="email" placeholder="Email" />
+          <input value={email} onChange={changeEvent} name="email" type="email" className="form-control" id="email" placeholder="Email" />
         </div>
       </div>
       <div className="row mb-3">
 
         <div className="col-sm-12">
-          <input onChange={changeEvent} value={data.password} name="password" type="password" className="form-control" id="password" placeholder="Password" />
+          <input onChange={changeEvent} value={password} name="password" type="password" className="form-control" id="password" placeholder="Password" />
         </div>
       </div>
       <div className="row mb-3">
         <div className="col-sm-12">
-          <input onChange={changeEvent} value={data.cpassword} name="cpassword" type="password" className="form-control" id="cpasssword" placeholder="Confirm password" />
+          <input onChange={changeEvent} value={cpassword} name="cpassword" type="password" className="form-control" id="cpasssword" placeholder="Confirm password" />
         </div>
       </div>
       <div className="row mb-3">
@@ -51,7 +77,7 @@ const changeEvent = (e) => {
       <div className="row mb-3">
         <div className="col-sm-12">
           <p className="text-center" >
-            Have an account? <Link style={{textDecoration: 'none'}} to="/signin">Sign In</Link>
+            Have an account? <Link style={{ textDecoration: 'none' }} to="/signin">Sign In</Link>
           </p>
         </div>
       </div>
@@ -61,6 +87,13 @@ const changeEvent = (e) => {
     <div className="signup-container">
       <div className="row vh-100">
         <div className="col-md-5 mx-auto my-auto" >
+          {/* {
+            errorMsg && 
+              setTimeout(() => setData({...data, errorMsg: false}), 3000)
+          } */}
+          {loading && <div className="text-center">{showLoading()}</div>}
+          {successMsg && showSuccessMsg(successMsg)}
+          {errorMsg && showErrorMsg(errorMsg)}
           {showSignUp()}
         </div>
       </div>
